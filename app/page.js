066dce8994dc0af08430e4bab40ca37851f2e2e9
Michael Palmer <mpalm2025@gmail.com>
2026-09-05 08:36:58 -0400

    Render Instagram footer

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import inventory from '../data/inventory.json';
 import InventoryExplorer from '../components/InventoryExplorer.js';
+import SiteFooter from '../components/SiteFooter.js';
 
 export default function HomePage() {
-  return <InventoryExplorer inventory={inventory} />;
+  return (
+    <>
+      <InventoryExplorer inventory={inventory} />
+      <SiteFooter />
+    </>
+  );
 }
